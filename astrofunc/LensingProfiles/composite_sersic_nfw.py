101eb59526924edbf287faae999e5b1ff259ca52
Simon Birrer <simon.birrer@pyhs.ethz.ch>
2017-07-28 15:47:35 -0700

    reparameterized M/L definition in composite model

--- astrofunc/LensingProfiles/composite_sersic_nfw.py
+++ astrofunc/LensingProfiles/composite_sersic_nfw.py
@@ -99,13 +99,16 @@ class CompositeSersicNFW(object):
         """
         alpha_E_sersic, _ = self.sersic.derivatives(theta_E, 0, n_sersic, r_eff, k_eff=1)
         alpha_E_nfw, _ = self.nfw.derivatives(theta_E, 0, Rs, theta_Rs=1, q=1, phi_G=0)
-        f_xx_s, f_yy_s, _ = self.sersic.hessian(r_eff, 0, n_sersic, r_eff, k_eff=1)
-        f_xx_n, f_yy_n, _ = self.nfw.hessian(r_eff, 0, Rs, theta_Rs=1, q=1, phi_G=0)
-        kappa_eff_sersic = (f_xx_s + f_yy_s) / 2.
-        kappa_eff_nfw = (f_xx_n + f_yy_n) / 2.
+        #f_xx_s, f_yy_s, _ = self.sersic.hessian(r_eff, 0, n_sersic, r_eff, k_eff=1)
+        #f_xx_n, f_yy_n, _ = self.nfw.hessian(r_eff, 0, Rs, theta_Rs=1, q=1, phi_G=0)
+        #kappa_eff_sersic = (f_xx_s + f_yy_s) / 2.
+        #kappa_eff_nfw = (f_xx_n + f_yy_n) / 2.
         # equations must satisfy:
         # theta_Rs * alpha_E_nfw + k_eff * alpha_E_sersic = theta_E
         # theta_Rs * kappa_eff_nfw / (k_eff * kappa_eff_sersic) = mass_light
-        k_eff = theta_E * kappa_eff_nfw / (alpha_E_sersic*kappa_eff_nfw+mass_light*alpha_E_nfw*kappa_eff_sersic)
-        theta_Rs = (theta_E - k_eff*alpha_E_sersic)/alpha_E_nfw
+        #k_eff = theta_E * kappa_eff_nfw / (alpha_E_sersic*kappa_eff_nfw+mass_light*alpha_E_nfw*kappa_eff_sersic)
+
+        #theta_Rs = (theta_E - k_eff*alpha_E_sersic)/alpha_E_nfw
+        theta_Rs = theta_E/alpha_E_nfw / (1 + 1./mass_light)
+        k_eff = (theta_E - theta_Rs * alpha_E_nfw)/alpha_E_sersic
         return theta_Rs, k_eff
--- test/test_composite.py
+++ test/test_composite.py
@@ -1,6 +1,7 @@
 from astrofunc.LensingProfiles.composite_sersic_nfw import CompositeSersicNFW
 from astrofunc.LensingProfiles.sersic import Sersic
 from astrofunc.LensingProfiles.nfw_ellipse import NFW_ELLIPSE
+import numpy.testing as npt
 
 
 class TestMassAngleConversion(object):
@@ -22,14 +23,11 @@ class TestMassAngleConversion(object):
 
         alpha_E_sersic, _ = self.sersic.derivatives(theta_E, 0, n_sersic, r_eff, k_eff=1)
         alpha_E_nfw, _ = self.nfw.derivatives(theta_E, 0, Rs, theta_Rs=1, q=1, phi_G=0)
-        f_xx_s, f_yy_s, _ = self.sersic.hessian(r_eff, 0, n_sersic, r_eff, k_eff=1)
-        f_xx_n, f_yy_n, _ = self.nfw.hessian(r_eff, 0, Rs, theta_Rs=1, q=1, phi_G=0)
-        kappa_eff_sersic = (f_xx_s + f_yy_s) / 2.
-        kappa_eff_nfw = (f_xx_n + f_yy_n) / 2.
-        a = theta_Rs * alpha_E_nfw + k_eff * alpha_E_sersic
-        b = theta_Rs * kappa_eff_nfw / (k_eff * kappa_eff_sersic)
-        assert a == theta_E
-        assert b == mass_light
+        a = theta_Rs * alpha_E_nfw + (k_eff * alpha_E_sersic)
+        b = theta_Rs * alpha_E_nfw / (k_eff * alpha_E_sersic)
+        npt.assert_almost_equal(a, theta_E, decimal=10)
+        npt.assert_almost_equal(b, mass_light, decimal=10)
+
 
 
 
