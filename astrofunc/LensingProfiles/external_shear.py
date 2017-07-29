sersic fixed, new test routines and numerical differentials

astrofunc/LensingProfiles/external_shear.py
@@ -91,8 +91,8 @@ class ExternalShear(object):
 
     def derivatives(self, x, y, e1, e2):
         # rotation angle
-        f_x = e1*x - e2*y
-        f_y = -e2*x - e1*y
+        f_x = e1*x + e2*y
+        f_y = +e2*x - e1*y
         return f_x, f_y
 
     def hessian(self, x, y, e1, e2):
@@ -108,8 +108,8 @@ class ExternalShear(object):
         psi_ext, gamma_ext = util.ellipticity2phi_gamma(e1, e2)
         theta, phi = util.cart2polar(x, y)
         f_ = 1./2 * gamma_ext * theta * np.cos(2*(phi - psi_ext))
-        f_x = e1*x - e2*y
-        f_y = -e2*x - e1*y
+        f_x = e1*x + e2*y
+        f_y = e2*x - e1*y
         gamma1 = e1
         gamma2 = e2
         kappa = 0

astrofunc/LensingProfiles/sersic_utils.py
@@ -94,9 +94,17 @@ class SersicUtil(object):
         :param center_y:
         :return:
         """
-        p = self._p(x, y, n_sersic, r_eff, k_eff, center_x, center_y)
-        q = self._q(x, y, n_sersic, r_eff, k_eff, center_x, center_y)
-        return -(p + q)
+        _dr = 0.00001
+        x_ = x - center_x
+        y_ = y - center_y
+        r = np.sqrt(x_**2 + y_**2)
+        alpha = self.alpha_abs(r, 0, n_sersic, r_eff, k_eff)
+        alpha_dr = self.alpha_abs(r+_dr, 0, n_sersic, r_eff, k_eff)
+        d_alpha_dr = (alpha_dr - alpha)/_dr
+        #p = self._p(x, y, n_sersic, r_eff, k_eff, center_x, center_y)
+        #q = self._q(x, y, n_sersic, r_eff, k_eff, center_x, center_y)
+        #return -(p + q)
+        return d_alpha_dr
 
     def _p(self, x, y, n_sersic, r_eff, k_eff, center_x=0, center_y=0):
         """

test/test_external_shear.py
@@ -38,14 +38,14 @@ class TestExternalShear(object):
         x = np.array([1])
         y = np.array([2])
         f_x, f_y = self.extShear.derivatives(x, y, **self.kwargs_lens)
-        npt.assert_almost_equal(f_x[0], -0.1, decimal=5)
-        npt.assert_almost_equal(f_y[0], -0.3, decimal=5)
+        npt.assert_almost_equal(f_x[0], 0.3, decimal=5)
+        npt.assert_almost_equal(f_y[0], -0.1, decimal=5)
 
         x = np.array([1, 3, 4])
         y = np.array([2, 1, 1])
         values = self.extShear.derivatives(x, y, **self.kwargs_lens)
-        npt.assert_almost_equal(values[0][0], -0.1, decimal=5)
-        npt.assert_almost_equal(values[1][0], -0.3, decimal=5)
+        npt.assert_almost_equal(values[0][0], 0.3, decimal=5)
+        npt.assert_almost_equal(values[1][0], -0.1, decimal=5)
 
     def test_hessian(self):
         x = np.array([1])
@@ -68,8 +68,8 @@ class TestExternalShear(object):
 
         f_, f_x, f_y, f_xx, f_yy, f_xy = self.extShear.all(x, y, **self.kwargs_lens)
         npt.assert_almost_equal(f_[0], 0.022360679774997925, decimal=5)
-        npt.assert_almost_equal(f_x[0], -0.1000000000000000, decimal=5)
-        npt.assert_almost_equal(f_y[0], -0.3, decimal=5)
+        npt.assert_almost_equal(f_x[0], 0.3, decimal=5)
+        npt.assert_almost_equal(f_y[0], -0.1, decimal=5)
         npt.assert_almost_equal(f_xx, 0.1, decimal=5)
 
 if __name__ == '__main__':

test/test_sersic_lens.py
@@ -1,6 +1,6 @@
 __author__ = 'sibirrer'
 
-
+import astrofunc.LensingProfiles.calc_util as calc_util
 from astrofunc.LensingProfiles.sersic import Sersic
 from astrofunc.LightProfiles.sersic import Sersic as Sersic_light
 
@@ -62,6 +62,26 @@ class TestSersic(object):
         assert values[0][1] == 0.2772992378623737
         assert values[1][1] == 0.092433079287457892
 
+    def test_differentails(self):
+        x_, y_ = 1., 1
+        n_sersic = 2.
+        r_eff = 1.
+        k_eff = 0.2
+        r = np.sqrt(x_**2 + y_**2)
+        d_alpha_dr = self.sersic.d_alpha_dr(x_, y_, n_sersic, r_eff, k_eff)
+        alpha = self.sersic.alpha_abs(x_, y_, n_sersic, r_eff, k_eff)
+
+        f_xx_ = d_alpha_dr * calc_util.d_r_dx(x_, y_) * x_/r + alpha * calc_util.d_x_diffr_dx(x_, y_)
+        f_yy_ = d_alpha_dr * calc_util.d_r_dy(x_, y_) * y_/r + alpha * calc_util.d_y_diffr_dy(x_, y_)
+        f_xy_ = d_alpha_dr * calc_util.d_r_dy(x_, y_) * x_/r + alpha * calc_util.d_x_diffr_dy(x_, y_)
+
+        f_xx = (d_alpha_dr/r - alpha/r**2) * y_**2/r + alpha/r
+        f_yy = (d_alpha_dr/r - alpha/r**2) * x_**2/r + alpha/r
+        f_xy = (d_alpha_dr/r - alpha/r**2) * x_*y_/r
+        npt.assert_almost_equal(f_xx, f_xx_, decimal=10)
+        npt.assert_almost_equal(f_yy, f_yy_, decimal=10)
+        npt.assert_almost_equal(f_xy, f_xy_, decimal=10)
+
     def test_hessian(self):
         x = np.array([1])
         y = np.array([2])
@@ -69,18 +89,18 @@ class TestSersic(object):
         r_eff = 1.
         k_eff = 0.2
         f_xx, f_yy,f_xy = self.sersic.hessian(x, y, n_sersic, r_eff, k_eff)
-        assert f_xx[0] == 0.15125869945524892
-        npt.assert_almost_equal(f_yy[0], -0.086355912401166718, decimal=10)
-        npt.assert_almost_equal(f_xy[0], -0.15840974123761042, decimal=10)
+        assert f_xx[0] == 0.1123170666045793
+        npt.assert_almost_equal(f_yy[0], -0.047414082641598576, decimal=10)
+        npt.assert_almost_equal(f_xy[0], -0.10648743283078525 , decimal=10)
         x = np.array([1,3,4])
         y = np.array([2,1,1])
         values = self.sersic.hessian(x, y, n_sersic, r_eff, k_eff)
-        assert values[0][0] == 0.15125869945524892
-        npt.assert_almost_equal(values[1][0], -0.086355912401166718, decimal=10)
-        npt.assert_almost_equal(values[2][0], -0.15840974123761042, decimal=10)
-        npt.assert_almost_equal(values[0][1], -0.071649513200062631, decimal=10)
-        npt.assert_almost_equal(values[1][1], 0.094619015499099512, decimal=10)
-        npt.assert_almost_equal(values[2][1], -0.062350698262185797, decimal=10)
+        assert values[0][0] == 0.1123170666045793
+        npt.assert_almost_equal(values[1][0], -0.047414082641598576, decimal=10)
+        npt.assert_almost_equal(values[2][0], -0.10648743283078525 , decimal=10)
+        npt.assert_almost_equal(values[0][1], -0.053273787681591328, decimal=10)
+        npt.assert_almost_equal(values[1][1], 0.076243427402007985, decimal=10)
+        npt.assert_almost_equal(values[2][1], -0.048568955656349749, decimal=10)
 
     def test_all(self):
         x = np.array([1])
@@ -92,41 +112,33 @@ class TestSersic(object):
         npt.assert_almost_equal(f_[0], 1.0272982586319199, decimal=10)
         assert f_x[0] == 0.16556078301997193
         assert f_y[0] == 0.33112156603994386
-        assert f_xx[0] == 0.15125869945524892
-        npt.assert_almost_equal(f_yy[0], -0.086355912401166718, decimal=10)
-        npt.assert_almost_equal(f_xy[0], -0.15840974123761042, decimal=10)
+        assert f_xx[0] == 0.1123170666045793
+        npt.assert_almost_equal(f_yy[0], -0.047414082641598576, decimal=10)
+        npt.assert_almost_equal(f_xy[0], -0.10648743283078525, decimal=10)
 
-    def test_magnificaton(self):
-        """
-
-        :return:
-        """
-        r = 5.
-        angle = 0.
-        x = r * np.cos(angle)
-        y = r * np.sin(angle)
+    def test_alpha_abs(self):
+        x = 1.
+        dr = 0.0000001
         n_sersic = 2.5
-        r_eff = 2.5
-        k_eff = 0.8
-        f_, f_x, f_y, f_xx, f_yy, f_xy = self.sersic.all(x, y, n_sersic, r_eff, k_eff)
-        mag = (1. - f_xx) * (1. - f_yy) - f_xy**2
+        r_eff = .5
+        k_eff = 0.2
+        alpha_abs = self.sersic.alpha_abs(x, 0, n_sersic, r_eff, k_eff)
+        f_dr = self.sersic.function(x + dr, 0, n_sersic, r_eff, k_eff)
+        f_ = self.sersic.function(x, 0, n_sersic, r_eff, k_eff)
+        alpha_abs_num = -(f_dr - f_)/dr
+        npt.assert_almost_equal(alpha_abs_num, alpha_abs, decimal=3)
 
-        p = self.sersic._p(x, y, n_sersic, r_eff, k_eff)
-        q = self.sersic._q(x, y, n_sersic, r_eff, k_eff)
-        A = (1-p)*(1+p+q)
-        print(mag, A, 'mag, A')
-        npt.assert_almost_equal(mag, A, decimal=10)
-
-        d_alpha_dr = self.sersic.d_alpha_dr(x, y, n_sersic, r_eff, k_eff)
-        assert -d_alpha_dr == p+q
-        alpha = self.sersic.alpha_abs(x, y, n_sersic, r_eff, k_eff)
-        npt.assert_almost_equal(alpha/r, p, decimal=10)
-        mag_new = (1 - alpha / r) * (1 - d_alpha_dr)
-        npt.assert_almost_equal(mag_new, A, decimal=10)
-        print(mag)
-
-        print(A, 'A')
-        npt.assert_almost_equal(mag, A, decimal=10)
+    def test_dalpha_dr(self):
+        x = 1.
+        dr = 0.0000001
+        n_sersic = 1.
+        r_eff = .5
+        k_eff = 0.2
+        d_alpha_dr = self.sersic.d_alpha_dr(x, 0, n_sersic, r_eff, k_eff)
+        alpha_dr = self.sersic.alpha_abs(x + dr, 0, n_sersic, r_eff, k_eff)
+        alpha = self.sersic.alpha_abs(x, 0, n_sersic, r_eff, k_eff)
+        d_alpha_dr_num = (alpha_dr - alpha)/dr
+        npt.assert_almost_equal(d_alpha_dr, d_alpha_dr_num, decimal=3)
 
     def test_mag_sym(self):
         """
@@ -169,7 +181,7 @@ class TestSersic(object):
         flux = self.sersic_light.function(x, y, I0_sersic=1., R_sersic=r_eff, n_sersic=n_sersic)
         flux /= flux[0]
         kappa /= kappa[0]
-        npt.assert_almost_equal(flux[1], kappa[1], decimal=10)
+        npt.assert_almost_equal(flux[1], kappa[1], decimal=5)
 
 if __name__ == '__main__':
     pytest.main()