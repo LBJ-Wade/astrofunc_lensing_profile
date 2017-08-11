a98014c1fe55405fb3001b4b93fd75fb463ef196
Simon Birrer <simon.birrer@pyhs.ethz.ch>
2017-08-11 13:36:38 -0700

    light profile ellipticities changed

--- astrofunc/LightProfiles/hernquist.py
+++ astrofunc/LightProfiles/hernquist.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class Hernquist(object):
@@ -23,7 +24,7 @@ class Hernquist(object):
         rho0 = self.lens.sigma2rho(sigma0, Rs)
         return self.lens.density_2d(x, y, rho0, Rs, center_x, center_y)
 
-    def light_3d(self, r, sigma0, Rs, center_x=0, center_y=0):
+    def light_3d(self, r, sigma0, Rs):
         """
 
         :param y:
@@ -42,7 +43,7 @@ class Hernquist_Ellipse(object):
     class for elliptical pseudo Jaffe lens light (2d projected light/mass distribution
     """
     def __init__(self):
-        from astrofunc.LensingProfiles.hernquist_ellipse import Hernquist_Ellipse as Hernquist_lens
+        from astrofunc.LensingProfiles.hernquist import Hernquist as Hernquist_lens
         self.lens = Hernquist_lens()
 
     def function(self, x, y, sigma0, Rs, q, phi_G, center_x=0, center_y=0):
@@ -57,5 +58,39 @@ class Hernquist_Ellipse(object):
         :param center_y:
         :return:
         """
-        f_xx, f_yy, _ = self.lens.hessian(x, y, sigma0, Rs, q, phi_G, center_x, center_y)
+        x_ , y_ = self._coord_transf(x, y, q, phi_G, center_x, center_y)
+        f_xx, f_yy, _ = self.lens.hessian(x_, y_, sigma0, Rs)
         return 1./2. * (f_xx + f_yy)
+
+    def light_3d(self, r, sigma0, Rs, q=1, phi_G=0):
+        """
+
+        :param y:
+        :param sigma0:
+        :param Rs:
+        :param center_x:
+        :param center_y:
+        :return:
+        """
+        rho0 = self.lens.sigma2rho(sigma0, Rs)
+        return self.lens.density(r, rho0, Rs)
+
+    def _coord_transf(self, x, y, q, phi_G, center_x, center_y):
+        """
+
+        :param x:
+        :param y:
+        :param q:
+        :param phi_G:
+        :param center_x:
+        :param center_y:
+        :return:
+        """
+        x_shift = x - center_x
+        y_shift = y - center_y
+        cos_phi = np.cos(phi_G)
+        sin_phi = np.sin(phi_G)
+        e = abs(1 - q)
+        x_ = (cos_phi * x_shift + sin_phi * y_shift) * np.sqrt(1 - e)
+        y_ = (-sin_phi * x_shift + cos_phi * y_shift) * np.sqrt(1 + e)
+        return x_, y_
--- astrofunc/LightProfiles/p_jaffe.py
+++ astrofunc/LightProfiles/p_jaffe.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 class PJaffe(object):
@@ -7,7 +7,7 @@ class PJaffe(object):
     """
     def __init__(self):
         from astrofunc.LensingProfiles.p_jaffe import PJaffe as PJaffe_lens
-        self.p_Jaffe = PJaffe_lens()
+        self.lens = PJaffe_lens()
 
     def function(self, x, y, sigma0, Ra, Rs, center_x=0, center_y=0):
         """
@@ -21,15 +21,28 @@ class PJaffe(object):
         :param center_y:
         :return:
         """
-        return self.p_Jaffe.density_2d(x, y, sigma0, Ra, Rs, center_x, center_y)
+        return self.lens.density_2d(x, y, sigma0, Ra, Rs, center_x, center_y)
+
+    def light_3d(self, r, sigma0, Ra, Rs, q=1, phi_G=0):
+        """
+
+        :param y:
+        :param sigma0:
+        :param Rs:
+        :param center_x:
+        :param center_y:
+        :return:
+        """
+        rho0 = self.lens.sigma2rho(sigma0, Ra, Rs)
+        return self.lens.density(r, rho0, Ra, Rs)
 
 class PJaffe_Ellipse(object):
     """
     calss for elliptical pseudo Jaffe lens light
     """
     def __init__(self):
-        from astrofunc.LensingProfiles.p_jaffe_ellipse import PJaffe_Ellipse as PJaffe_lens
-        self.p_Jaffe = PJaffe_lens()
+        from astrofunc.LensingProfiles.p_jaffe import PJaffe as PJaffe_lens
+        self.lens = PJaffe_lens()
 
     def function(self, x, y, sigma0, Ra, Rs, q, phi_G, center_x=0, center_y=0):
         """
@@ -43,5 +56,39 @@ class PJaffe_Ellipse(object):
         :param center_y:
         :return:
         """
-        f_xx, f_yy, _ = self.p_Jaffe.hessian(x, y, sigma0, Ra, Rs, q, phi_G, center_x, center_y)
+        x_, y_ = self._coord_transf(x, y, q, phi_G, center_x, center_y)
+        f_xx, f_yy, _ = self.lens.hessian(x_, y_, sigma0, Ra, Rs)
         return 1./2. * (f_xx + f_yy)
+
+    def light_3d(self, r, sigma0, Ra, Rs, q=1, phi_G=0):
+        """
+
+        :param y:
+        :param sigma0:
+        :param Rs:
+        :param center_x:
+        :param center_y:
+        :return:
+        """
+        rho0 = self.lens.sigma2rho(sigma0, Ra, Rs)
+        return self.lens.density(r, rho0, Ra, Rs)
+
+    def _coord_transf(self, x, y, q, phi_G, center_x, center_y):
+        """
+
+        :param x:
+        :param y:
+        :param q:
+        :param phi_G:
+        :param center_x:
+        :param center_y:
+        :return:
+        """
+        x_shift = x - center_x
+        y_shift = y - center_y
+        cos_phi = np.cos(phi_G)
+        sin_phi = np.sin(phi_G)
+        e = abs(1 - q)
+        x_ = (cos_phi * x_shift + sin_phi * y_shift) * np.sqrt(1 - e)
+        y_ = (-sin_phi * x_shift + cos_phi * y_shift) * np.sqrt(1 + e)
+        return x_, y_
