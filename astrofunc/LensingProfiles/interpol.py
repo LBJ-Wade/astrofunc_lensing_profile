308e8c69328650aeb5e5da3b403a77fff662fcb1
Simon Birrer <simon.birrer@pyhs.ethz.ch>
2017-10-12 13:13:30 -0700

    interpolated lens model with loops computed

--- astrofunc/LensingProfiles/interpol.py
+++ astrofunc/LensingProfiles/interpol.py
@@ -9,15 +9,23 @@ class Interpol_func(object):
     """
     class which uses an interpolation of a lens model and its first and second order derivatives
     """
+    def __init__(self, grid=True):
+        self._grid = grid
 
     def function(self, x, y):
         if type(x) == float or type(x) == int or type(x) == type(np.float64(1)) or len(x) <= 1:
             f_ = self.f_interp(y, x)
             return f_[0][0]
         else:
-            x_axes, y_axes = util.get_axes(x, y)
-            f_ = self.f_interp(y_axes, x_axes)
-            f_ = util.image2array(f_)
+            if self._grid:
+                x_axes, y_axes = util.get_axes(x, y)
+                f_ = self.f_interp(y_axes, x_axes)
+                f_ = util.image2array(f_)
+            else:
+                n = len(x)
+                f_ = np.zeros(n)
+                for i in range(n):
+                    f_[i] = self.f_interp(y[i], x[i])
         return f_
 
     def derivatives(self, x, y):
@@ -29,11 +37,18 @@ class Interpol_func(object):
             f_y = self.f_y_interp(y, x)
             return f_x[0][0], f_y[0][0]
         else:
-            x_, y_ = util.get_axes(x, y)
-            f_x = self.f_x_interp(y_, x_)
-            f_y = self.f_y_interp(y_, x_)
-            f_x = util.image2array(f_x)
-            f_y = util.image2array(f_y)
+            if self._grid:
+                x_, y_ = util.get_axes(x, y)
+                f_x = self.f_x_interp(y_, x_)
+                f_y = self.f_y_interp(y_, x_)
+                f_x = util.image2array(f_x)
+                f_y = util.image2array(f_y)
+            else:
+                n = len(x)
+                f_x, f_y = np.zeros(n), np.zeros(n)
+                for i in range(n):
+                    f_x[i] = self.f_x_interp(y[i], x[i])
+                    f_y[i] = self.f_y_interp(y[i], x[i])
         return f_x, f_y
 
     def hessian(self, x, y):
@@ -46,13 +61,21 @@ class Interpol_func(object):
             f_xy = self.f_xy_interp(y, x)
             return f_xx[0][0], f_yy[0][0], f_xy[0][0]
         else:
-            x_, y_ = util.get_axes(x, y)
-            f_xx = self.f_xx_interp(y_, x_)
-            f_yy = self.f_yy_interp(y_, x_)
-            f_xy = self.f_xy_interp(y_, x_)
-            f_xx = util.image2array(f_xx)
-            f_yy = util.image2array(f_yy)
-            f_xy = util.image2array(f_xy)
+            if self._grid:
+                x_, y_ = util.get_axes(x, y)
+                f_xx = self.f_xx_interp(y_, x_)
+                f_yy = self.f_yy_interp(y_, x_)
+                f_xy = self.f_xy_interp(y_, x_)
+                f_xx = util.image2array(f_xx)
+                f_yy = util.image2array(f_yy)
+                f_xy = util.image2array(f_xy)
+            else:
+                n = len(x)
+                f_xx, f_yy, f_xy = np.zeros(n), np.zeros(n), np.zeros(n)
+                for i in range(n):
+                    f_xx[i] = self.f_xx_interp(y[i], x[i])
+                    f_yy[i] = self.f_yy_interp(y[i], x[i])
+                    f_xy[i] = self.f_xy_interp(y[i], x[i])
         return f_xx, f_yy, f_xy
 
     def all(self, x, y):
@@ -68,19 +91,30 @@ class Interpol_func(object):
             f_xy = self.f_xy_interp(y, x)
             return f_[0][0], f_x[0][0], f_y[0][0], f_xx[0][0], f_yy[0][0], f_xy[0][0]
         else:
-            x_, y_ = util.get_axes(x, y)
-            f_ = self.f_interp(y_, x_)
-            f_x = self.f_x_interp(y_, x_)
-            f_y = self.f_y_interp(y_, x_)
-            f_xx = self.f_xx_interp(y_, x_)
-            f_yy = self.f_yy_interp(y_, x_)
-            f_xy = self.f_xy_interp(y_, x_)
-            f_ = util.image2array(f_)
-            f_x = util.image2array(f_x)
-            f_y = util.image2array(f_y)
-            f_xx = util.image2array(f_xx)
-            f_yy = util.image2array(f_yy)
-            f_xy = util.image2array(f_xy)
+            if self._grid:
+                x_, y_ = util.get_axes(x, y)
+                f_ = self.f_interp(y_, x_)
+                f_x = self.f_x_interp(y_, x_)
+                f_y = self.f_y_interp(y_, x_)
+                f_xx = self.f_xx_interp(y_, x_)
+                f_yy = self.f_yy_interp(y_, x_)
+                f_xy = self.f_xy_interp(y_, x_)
+                f_ = util.image2array(f_)
+                f_x = util.image2array(f_x)
+                f_y = util.image2array(f_y)
+                f_xx = util.image2array(f_xx)
+                f_yy = util.image2array(f_yy)
+                f_xy = util.image2array(f_xy)
+            else:
+                n = len(x)
+                f_, f_x, f_y, f_xx, f_yy, f_xy = np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n)
+                for i in range(n):
+                    f_[i] = self.f_interp(y[i], x[i])
+                    f_x[i] = self.f_x_interp(y[i], x[i])
+                    f_y[i] = self.f_y_interp(y[i], x[i])
+                    f_xx[i] = self.f_xx_interp(y[i], x[i])
+                    f_yy[i] = self.f_yy_interp(y[i], x[i])
+                    f_xy[i] = self.f_xy_interp(y[i], x[i])
         return f_, f_x, f_y, f_xx, f_yy, f_xy
 
     def do_interp(self, x_grid, y_grid, f_, f_x, f_y, f_xx, f_yy, f_xy):
--- test/test_interpol.py
+++ test/test_interpol.py
@@ -17,22 +17,33 @@ class TestInterpol(object):
         f_sis, f_x_sis, f_y_sis, f_xx_sis, f_yy_sis, f_xy_sis = sis.all(x_grid_interp, y_grid_interp, **kwargs_SIS)
         x_axes, y_axes = util.get_axes(x_grid_interp, y_grid_interp)
         interp_func = Interpol_func()
+        interp_func_loop = Interpol_func(grid=False)
         interp_func.do_interp(x_axes, y_axes, util.array2image(f_sis), util.array2image(f_x_sis), util.array2image(f_y_sis), util.array2image(f_xx_sis), util.array2image(f_yy_sis), util.array2image(f_xy_sis))
+        interp_func_loop.do_interp(x_axes, y_axes, util.array2image(f_sis), util.array2image(f_x_sis), util.array2image(f_y_sis), util.array2image(f_xx_sis), util.array2image(f_yy_sis), util.array2image(f_xy_sis))
 
         # test derivatives
         assert interp_func.derivatives(1,0) == sis.derivatives(1,0, **kwargs_SIS)
+        assert interp_func.derivatives(1, 0) == interp_func_loop.derivatives(1, 0)
         alpha1_interp, alpha2_interp = interp_func.derivatives(np.array([0,1,0,1]), np.array([1,1,2,2]))
+        alpha1_interp_loop, alpha2_interp_loop = interp_func_loop.derivatives(np.array([0, 1, 0, 1]), np.array([1, 1, 2, 2]))
         alpha1_true, alpha2_true = sis.derivatives(np.array([0,1,0,1]),np.array([1,1,2,2]), **kwargs_SIS)
         assert alpha1_interp[0] == alpha1_true[0]
         assert alpha1_interp[1] == alpha1_true[1]
+        assert alpha1_interp[0] == alpha1_interp_loop[0]
+        assert alpha1_interp[1] == alpha1_interp_loop[1]
         # test hessian
         assert interp_func.hessian(1,0) == sis.hessian(1,0, **kwargs_SIS)
         f_xx_interp, f_yy_interp, f_xy_interp = interp_func.hessian(np.array([0,1,0,1]), np.array([1,1,2,2]))
+        f_xx_interp_loop, f_yy_interp_loop, f_xy_interp_loop = interp_func_loop.hessian(np.array([0, 1, 0, 1]), np.array([1, 1, 2, 2]))
         f_xx_true, f_yy_true, f_xy_true = sis.hessian(np.array([0,1,0,1]),np.array([1,1,2,2]), **kwargs_SIS)
         assert f_xx_interp[0] == f_xx_true[0]
         assert f_xx_interp[1] == f_xx_true[1]
         assert f_xy_interp[0] == f_xy_true[0]
         assert f_xy_interp[1] == f_xy_true[1]
+        assert f_xx_interp[0] == f_xx_interp_loop[0]
+        assert f_xx_interp[1] == f_xx_interp_loop[1]
+        assert f_xy_interp[0] == f_xy_interp_loop[0]
+        assert f_xy_interp[1] == f_xy_interp_loop[1]
         # test all
         assert interp_func.all(1,0) == sis.all(1,0, **kwargs_SIS)
         f_interp, f_x_interp, f_y_interp, f_xx_interp, f_yy_interp, f_xy_interp = interp_func.all(np.array([0,1,0,1]), np.array([1,1,2,2]))
