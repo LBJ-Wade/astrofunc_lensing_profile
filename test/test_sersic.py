00df97b6ebf54bf27743e15d8dc888931f91abe4
Simon Birrer <simon.birrer@pyhs.ethz.ch>
2017-12-23 23:17:37 +0100

    outsourced fft convolution from util class

--- test/test_sersic.py
+++ test/test_sersic.py
@@ -131,18 +131,18 @@ class TestSersic(object):
         center_y = 0
         smoothing = 0.02
         values = self.double_sersic.function(x, y, I0_sersic, R_sersic, n_sersic, phi_G, q, I0_2, R_2, n_2, center_x, center_y)
-        assert values[0] == 0.20675084739923477
+        assert values[0] == 0.20696126663199443
         x = np.array([0])
         y = np.array([0])
         values = self.double_sersic.function(x, y, I0_sersic, R_sersic, n_sersic, phi_G, q, I0_2, R_2, n_2, center_x, center_y)
-        assert values[0] == 7.8708484172821045
+        assert values[0] == 7.8708488164502022
 
         x = np.array([2,3,4])
         y = np.array([1,1,1])
         values = self.double_sersic.function(x, y, I0_sersic, R_sersic, n_sersic, phi_G, q, I0_2, R_2, n_2, center_x, center_y)
-        npt.assert_almost_equal(values[0], 0.18951603025197983, decimal=8)
-        npt.assert_almost_equal(values[1], 0.056245995685458183, decimal=8)
-        npt.assert_almost_equal(values[2], 0.021287990035489202, decimal=8)
+        npt.assert_almost_equal(values[0], 0.19409037374964733, decimal=8)
+        npt.assert_almost_equal(values[1], 0.060052096255106595, decimal=8)
+        npt.assert_almost_equal(values[2], 0.023917479151437715, decimal=8)
 
 if __name__ == '__main__':
     pytest.main()
